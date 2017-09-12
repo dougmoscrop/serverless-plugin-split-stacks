a2b433449b84e9cf236be3819c56f3f76cd45bde
Doug Moscrop <doug.moscrop@gmail.com>
2017-09-11 20:49:13 -0400

    Support stack suffixes (overflow) for Filters and Alarms

---
+++ __tests__/utils/get-stack-name.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const test = require('ava');
+const sinon = require('sinon');
+
+const utils = require('../../lib/utils');
+
+const fullResources = {};
+
+for (let i = 0; i < 200; i++) {
+	fullResources[i] = {};
+}
+
+test.beforeEach(t => {
+	t.context = Object.assign({}, utils);
+});
+
+test('returns destination (simple case)', t => {
+	const stackName = t.context.getStackName('Foo');
+	t.deepEqual(stackName, 'FooNestedStack');
+});
+
+test('throws when stack is full and allowSuffix is false', t => {
+	const stub = sinon.stub(t.context, 'nestedStack')
+		.onCall(0).returns({
+			Resources: fullResources
+		})
+		.onCall(1).returns({
+			Resources: {
+				Foo: {}
+			}
+		});
+
+	const err =	t.throws(() => t.context.getStackName('Foo'));
+	t.true(stub.calledOnce);
+	t.deepEqual(err.message, 'Destination stack Foo is already full!');
+});
+
+test('returns a suffixed name (one full stack)', t => {
+	const stub = sinon.stub(t.context, 'nestedStack')
+		.onCall(0).returns({
+			Resources: fullResources
+		})
+		.onCall(1).returns({
+			Resources: {
+				Foo: {}
+			}
+		});
+
+	const stackName = t.context.getStackName('Foo', true);
+	t.deepEqual(stackName, 'FooNestedStack2');
+	t.true(stub.calledTwice);
+});
+
+test('returns a suffixed name (two full stacks)', t => {
+	const stub = sinon.stub(t.context, 'nestedStack')
+		.onCall(0).returns({
+			Resources: fullResources
+		})
+		.onCall(1).returns({
+			Resources: fullResources
+		})
+		.onCall(2).returns({
+			Resources: {}
+		});
+
+	const stackName = t.context.getStackName('Foo', true);
+	t.deepEqual(stackName, 'FooNestedStack3');
+	t.true(stub.calledThrice);
+});
