6bef068b1cba28fa61f25342eb32397bf75e7e36
Mariusz Nowak <medyk@medikoo.com>
2017-10-16 15:56:16 +0200

    Move stacks map to outer file, so it's customizable

---
+++ split-stacks-map.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  'AWS::CloudWatch::Alarm': {
+    destination: 'Alarms',
+    allowSuffix: true
+  },
+  'AWS::Logs::MetricFilter': {
+    destination: 'Filters',
+    allowSuffix: true
+  },
+  'AWS::Logs::SubscriptionFilter': {
+    destination: 'Filters',
+    allowSuffix: true
+  },
+  'AWS::ApiGateway::Resource': {
+    destination: 'API'
+  },
+  'AWS::ApiGateway::RestApi': {
+    destination: 'API'
+  },
+  'AWS::Lambda::Version': {
+    destination: 'Versions'
+  },
+  'AWS::Lambda::Permission': {
+    destination: 'Permissions'
+  },
+  'AWS::SNS::Subscription': {
+    destination: 'Subscriptions'
+  },
+  'AWS::SNS::TopicPolicy': {
+    destination: 'Policies'
+  },
+  'AWS::S3::BucketPolicy': {
+    destination: 'Policies'
+  },
+  'AWS::SQS::QueuePolicy': {
+    destination: 'Policies'
+  }
+};  
