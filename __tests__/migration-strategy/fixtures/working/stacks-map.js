c1a67b8e1c2f30493cd790cffae43b28007b8901
Doug Moscrop <doug.moscrop@gmail.com>
2018-05-16 16:46:00 -0400

    add some tests for per-type

---
+++ __tests__/migration-strategy/fixtures/working/stacks-map.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  'AWS::CloudWatch::Alarm': {
+    destination: 'Custom'
+  },
+  'Foo::Bar': {}
+};
---
+++ __tests__/migration-strategy/per-type.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const sinon = require('sinon');
+const test = require('ava');
+
+const PerType = require('../../lib/migration-strategy/per-type');
+
+test('adds stacks-map that exists', t => {
+  const plugin = {
+    config: {},
+    stacksMap: {},
+    serverless: {
+      config: {
+        servicePath: `${__dirname}/fixtures/working`
+      }
+    }
+  };
+
+  const strategy = new PerType(plugin);
+  
+  t.deepEqual(strategy.migration({ Type: 'Foo::Bar' }), {});
+  t.deepEqual(strategy.migration({ Type: 'AWS::CloudWatch::Alarm' }), { destination: 'Custom' });
+});
+
+test('handles non-existing stacks-map', t => {
+  const plugin = {
+    config: {},
+    stacksMap: {},
+    serverless: {
+      config: {
+        servicePath: `${__dirname}/__does_not_exist__`
+      }
+    }
+  };
+
+  const strategy = new PerType(plugin);
+  
+  t.deepEqual(strategy.migration({ Type: 'AWS::CloudWatch::Alarm' }), { destination: 'Alarms', allowSuffix: true });
+});
+
+test('can disable per type defalts', t => {
+  const plugin = {
+    config: {
+      perType: false
+    },
+    stacksMap: {},
+    serverless: {
+      config: {
+        servicePath: `${__dirname}/fixtures/__does_not_exist__`
+      }
+    }
+  };
+
+  new PerType(plugin);
+
+  t.deepEqual(plugin.stacksMap, {});
+});
+
+test('throws when stacks-map does', t => {
+  const plugin = {
+    config: {},
+    stacksMap: {},
+    serverless: {
+      config: {
+        servicePath: `${__dirname}/fixtures/throwing`
+      }
+    }
+  };
+
+  t.throws(() => {
+    new PerType(plugin);
+  });
+});
