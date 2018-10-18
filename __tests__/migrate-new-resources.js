26bb50761ed813ba4a0541110def5eef5817245a
Doug Moscrop <doug.moscrop@gmail.com>
2018-10-18 16:23:35 -0400

    Support disabling migration from custom

--- __tests__/migrate-new-resources.js
+++ __tests__/migrate-new-resources.js
@@ -1,11 +1,22 @@
 'use strict';
 
+const proxyquire = require('proxyquire');
 const sinon = require('sinon');
 const test = require('ava');
 
-const migrateNewResources = require('../lib/migrate-new-resources');
-
 test.beforeEach(t => {
+  const customMigrationStub = sinon.stub();
+
+  class StubMigration {
+    migration() {
+      return customMigrationStub.apply(customMigrationStub, arguments);
+    }
+  }
+
+  const migrateNewResources = proxyquire('../lib/migrate-new-resources', {
+    './migration-strategy/custom': StubMigration
+  });
+
 	t.context = Object.assign({ resourceMigrations: {} }, { migrateNewResources }, {
 		config: {
 			perType: true
@@ -17,7 +28,8 @@ test.beforeEach(t => {
 		},
 		provider: {},
 		getStackName: () => 'test',
-		migrate: sinon.stub()
+		migrate: sinon.stub(),
+		customMigrationStub,
 	});
 });
 
@@ -48,4 +60,17 @@ test('does not call migrate for already migrated resources', t => {
 
 	t.context.migrateNewResources();
 	t.true(t.context.migrate.calledOnce);
+});
+
+test('does not migrate when custom returns false', t => {
+	t.context.resourcesById = {
+		Foo: {
+			Type: 'AWS::Logs::SubscriptionFilter'
+		},
+	};
+
+  t.context.customMigrationStub.returns(false);
+  t.context.migrateNewResources();
+  t.is(t.context.customMigrationStub.callCount, 1);
+	t.is(t.context.migrate.callCount, 0);
 });
